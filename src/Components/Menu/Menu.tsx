a7ca6e6d369da4278d76449185cdd7587128af48
MatheusGmota <livematgreg@gmail.com>
2024-08-27 18:22:55 -0300

    Alterando modo de iserir a imagem no heade

--- src/Components/Menu/Menu.tsx
+++ src/Components/Menu/Menu.tsx
@@ -1,11 +1,12 @@
 import { Link } from "react-router-dom";
 import styles from "../../styles.module.css"
 import Button from "../Button/Button";
+import logoImage from "/carrepair.svg"
 
 export default function Menu() {
     return (
       <nav className={styles.nav}>
-        <Link to="/"><img className={styles.logo} src="carrepair.svg" alt="Logo Carrepair" /></Link>
+        <Link to="/"><img className={styles.logo} src={logoImage} alt="Logo Carrepair" /></Link>
         <ul>
             <li><Link to="/pagina-participantes">Página dos Participantes</Link></li>
             <li><Link to="/orcamento">Orçamento</Link></li>
