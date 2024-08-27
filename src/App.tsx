3503cbfe8b1172ae7946d67bf24a54d23747c857
MatheusGmota <livematgreg@gmail.com>
2024-08-27 18:23:12 -0300

    Alterações mínimas

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Outlet } from "react-router-dom";
 import Header from "./Components/Header/Header";
 import Footer from "./Components/Footer/Footer";
+import "./index.css"
 
 export default function App() {
   return (
