a18e113f6e7c6b630e3542002392e8809cfdf306
MatheusGmota <livematgreg@gmail.com>
2024-08-28 17:24:54 -0300

    tipagem do componente Card

--- src/types.ts
+++ src/types.ts
@@ -1,6 +1,14 @@
 export interface ButtonInterface {
     text: string,
-    type: string,
+    type: 'primary'|'secondary'|'tertiary',
     href: string,
     filled?: boolean
+}
+export interface CardInterface {
+    image?: string,
+    icon?: JSX.Element,
+    title: string,
+    subtitle?: string,
+    hrefSocialMedia?: string[],
+    type: string
 }
