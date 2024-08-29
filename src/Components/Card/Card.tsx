16c16632c8d31584cbfe2a00747b8e64a8bfd262
MatheusGmota <livematgreg@gmail.com>
2024-08-29 14:52:10 -0300

    Reestruturando o componente Card

--- src/Components/Card/Card.tsx
+++ src/Components/Card/Card.tsx
@@ -3,28 +3,41 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faGithub, faInstagram, faLinkedin } from "@fortawesome/free-brands-svg-icons";
 import styles from "./Card.module.css"
 
-export const Card = ({image,icon,title,subtitle,type,hrefSocialMedia}:CardInterface) => {
+export const Card = ({image, icon, title, bodyText, description, type, hrefSocialMedia}:CardInterface) => {
+
     const [hrefGit, hrefInsta, hrefLkdin] = hrefSocialMedia ? hrefSocialMedia : "";
-    const [a,b] = subtitle? subtitle : ""
+
+    const [rm, turma] = bodyText? bodyText : ""
+
     return (
         <>
-            <article className={`${styles.card} ${styles[type]}`}>
-                {image &&
-                    <img src={image} alt="Imagem Random"  className={styles.image}/>}
-                {icon &&
-                    <div className={styles.iconCard}>{icon}</div>}
-                <div className={styles.body}>
-                    <h3 className={styles.title}>{title}</h3>
-                    <p>{a}</p>
-                    <p>{b}</p>
+            {type == 'primary' && 
+                <div className={`${styles.card} ${styles[type]}`}>
+                    <div className={styles.theFront}>
+                        <div className={styles.iconCard}>{icon}</div>
+                        <h3 className={styles.title}>{title}</h3>
+                    </div>
+                    <div className={styles.theBack}>
+                        <small>{description}</small>
+                    </div>
                 </div>
-                {hrefGit && hrefInsta && hrefLkdin &&
-                <div className={styles.socialMedias}>
-                    <a href={hrefGit}><FontAwesomeIcon icon={faGithub}/></a>
-                    <a href={hrefInsta}><FontAwesomeIcon icon={faInstagram}/></a>
-                    <a href={hrefLkdin}><FontAwesomeIcon icon={faLinkedin}/></a>
-                </div>}
-            </article>
+            }
+
+            {type == 'secondary' && 
+                <div className={`${styles.card} ${styles[type]}`}>
+                    <img src={image} alt="Imagem Random"  className={styles.image}/>
+                    <div className={styles.body}>
+                        <h3 className={styles.title}>{title}</h3>
+                        <small>{rm}</small>
+                        <small>{turma}</small>
+                    </div>
+                    <div className={styles.iconSocial}>
+                        <a href={hrefGit}><FontAwesomeIcon icon={faGithub}/></a>
+                        <a href={hrefInsta}><FontAwesomeIcon icon={faInstagram}/></a>
+                        <a href={hrefLkdin}><FontAwesomeIcon icon={faLinkedin}/></a>
+                    </div>
+                </div>
+            }
         </>
     )
 }
