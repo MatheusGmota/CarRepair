e3a7f3d542a6425fb04e36c9f2c6a71236d34f23
felipeSeiki <felipe.hashiguti@gmail.com>
2024-08-30 20:36:20 -0300

    Preparando os cards, so falta foto

--- src/routes/PaginaParticipantes/index.tsx
+++ src/routes/PaginaParticipantes/index.tsx
@@ -1,7 +1,43 @@
+import { Card } from "../../Components/Card/Card";
+
 export default function PaginaIntegrantes() {
-    return(
+    const turma: string = "Turma - 1TDSPY"
+
+    const felipe = { nome: "Felipe Seiki Hashiguti", rm: "98985", hrefs: ["https://github.com/felipeSeiki", "https://www.instagram.com/felipeseiki18/", "https://www.linkedin.com/in/felipe-seiki-hashiguti/"] }
+    const lucas = { nome: "Lucas Corradini Silveira", rm: "555118", hrefs: ["https://github.com/lu-corradini", "https://www.instagram.com/corradini_l12/", "https://www.linkedin.com/in/lucas-corradini-330856214/"] }
+    const matheus = { nome: "Matheus Gregorio Mota", rm: "557254", hrefs: ["https://github.com/MatheusGmota", "https://www.instagram.com/theusz.g?igsh=MXJjMXNmazFkcjY2ag==", "https://www.linkedin.com/in/matheus-g-mota-/"] }
+
+    const [gitFelipe, instaFelipe, lkdinFelipe] = felipe.hrefs
+    const [gitLucas, instaLucas, lkdinLucas] = lucas.hrefs
+    const [gitMatheus, instaMatheus, lkdinMatheus] = matheus.hrefs
+
+
+    return (
         <main>
             <h1>Página dos integrantes</h1>
+            <div>
+                <Card title={felipe.nome}
+                    type="secondary"
+                    bodyText={[felipe.rm, turma]}
+                    hrefGit={gitFelipe}
+                    hrefInsta={instaFelipe}
+                    hrefLkdin={lkdinFelipe}
+                />
+                <Card title={lucas.nome}
+                    type="secondary"
+                    bodyText={[lucas.rm, turma]}
+                    hrefGit={gitLucas}
+                    hrefInsta={instaLucas}
+                    hrefLkdin={lkdinLucas}
+                />
+                <Card title={matheus.nome}
+                    type="secondary"
+                    bodyText={[matheus.rm, turma]}
+                    hrefGit={gitMatheus}
+                    hrefInsta={instaMatheus}
+                    hrefLkdin={lkdinMatheus}
+                />
+            </div>
         </main>
     )
 }
