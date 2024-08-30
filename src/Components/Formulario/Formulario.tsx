ea64d342f168804b64091a1dd955f76c775d1fa4
MatheusGmota <livematgreg@gmail.com>
2024-08-30 16:18:19 -0300

    Criando componente Formulario

--- src/Components/Formulario/Formulario.tsx
+++ src/Components/Formulario/Formulario.tsx
@@ -1,22 +1,68 @@
+import {useForm, Resolver} from "react-hook-form"
+import styles from "./Formulario.module.css"
+import Button from "../Button/Button"
+
+type FormValues = {
+    placa: string,
+    marca: string,
+    modelo: string,
+    kilometragem: number,
+    ano: number,
+    chassi: string
+}
+
+const messageErr = "Esse campo é obrigatório*"
+
+const resolver: Resolver<FormValues> = async (values) => {
+    return {
+        values: values.placa ? values : {},
+        errors: !values.placa 
+        ? {
+            placa: {
+                type: "required",
+                message: messageErr
+            }
+        } : {},
+    }
+}
 export const Formulario = () => {
-  return (
-   <>
-    <form action="#">
-        <div>
-            <label htmlFor="">Informe a placa do seu veículo</label>
-            <input type="text" />
-            <label htmlFor="">Marca</label>
-            <input type="text" required/>
-            <label htmlFor="">Modelo</label>
-            <input type="text" required/>
-            <label htmlFor="">Kilometragem</label>
-            <input type="text" required/>
-            <label htmlFor="">Ano</label>
-            <input type="text" required/>
-            <label htmlFor="">Chassi</label>
-            <input type="text" />
-        </div>
-    </form>
-   </>
-  )
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+      } = useForm<FormValues>({ resolver })
+      const onSubmit = handleSubmit((data) => console.log(data))
+
+    return (
+        <form onSubmit={onSubmit} className={styles.formulario}>
+            <div>
+                <label htmlFor="">Placa</label>
+                <input {...register("placa")} />
+                {errors?.placa && <span className={styles.err}>{errors.placa.message}</span>}
+            </div>
+            <div>
+                <label htmlFor="">Marca</label>
+                <input {...register("marca")} />
+            </div>
+            <div>
+                <label htmlFor="">Modelo</label>
+                <input {...register("modelo")} />
+            </div>
+            <div>
+                <label htmlFor="">Kilometragem</label>
+                <input {...register("kilometragem")} />
+            </div>
+            <div>
+                <label htmlFor="">Ano</label>
+                <input {...register("ano")} />
+            </div>
+            <div>
+                <label htmlFor="">Chassi</label>
+                <input {...register("chassi")} />
+            </div>
+            <div>
+                <Button href="submit" text="Próximo" type="secondary" filled={true}/>
+            </div>
+        </form>
+    )
 }
