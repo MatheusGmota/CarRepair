746efad1c941ad010631ce2ce0927ddec1366d08
felipeSeiki <felipe.hashiguti@gmail.com>
2024-08-31 19:16:37 -0300

    Criando o bacjground e os componentes da página Cadastro

--- src/routes/Cadastro/index.tsx
+++ src/routes/Cadastro/index.tsx
@@ -1,7 +1,38 @@
+import estilo from "./Cadastro.module.css"
+
 export default function Cadastro() {
-    return (
-      <main>
+  return (
+    <main>
+      <div className={estilo.paper} style={{ display: "flex", flexDirection: "column" }}>
         <h1>Cadastre-se</h1>
-      </main>
-    )
+        <div className={estilo.inputs}>
+          <h2>
+            Nome
+            <input type="text" name="nome" placeholder="Digite seu nome" />
+          </h2>
+          <h2>
+            Sobrenome
+            <input type="text" name="sobrenome" placeholder="Digite seu sobrenome" />
+          </h2>
+          <h2>
+            Cpf
+            <input type="text" name="cpf" placeholder="xxx.xxx.xxx-xx" />
+          </h2>
+          <h2>
+            Telefone
+            <input type="text" name="telefone" placeholder="(xx) x xxxx - xxxx" />
+          </h2>
+          <h2>
+            E-mail
+            <input type="text" name="email" placeholder="Digite seu e-mail" />
+          </h2>
+          <h2>
+            Senha
+            <input type="text" name="senha" placeholder="Digite sua senha" />
+          </h2>
+
+        </div>
+      </div>
+    </main>
+  )
 }
