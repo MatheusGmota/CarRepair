3f784ad2d52a9d1afba1b4ade4242eca6f1ba97f
lucas-corradini <lucas.corradini.2003@gmail.com>
2024-09-12 13:52:09 -0300

    finalizado desktop pagina Orçamento

--- src/routes/Orçamento/index.tsx
+++ src/routes/Orçamento/index.tsx
@@ -1,7 +1,51 @@
+import Button from "../../Components/Button/Button";
+import estilo from "./Orcamento.module.css";
+
 export default function Orcamento() {
-    return(
-        <main>
-            <h1>Página do Orçamento</h1>
+    return (
+        <main className={estilo.main}>
+            <section className={estilo.mainSection}>
+                <article>
+                    <h2 className={estilo.h2}>
+                        Qual ferramenta deseja usar?
+                    </h2>
+                </article>
+                <section className={estilo.sectionBox}>
+                    <article>
+                        <div className={estilo.chat}>
+                            <article>
+                                <h3>
+                                    Chatbot
+                                </h3>
+                            </article>
+                            <ul>
+                                <li>Orçamentos precisos.</li>
+                                <li>Esclarece dúvidas técnicas.</li>
+                                <li>Ideal para quem desconhece o problema do veículo.</li>
+                                <li>Rápido e conveniente.</li>
+                            </ul>
+                            <article>
+                                <Button type="primary" href="/orcamento/chatbot/:id" text="Gerar Orçamento" filled={true}/>
+                            </article>
+                        </div>
+                        <div className={estilo.forms}>
+                            <article>
+                                <h3>
+                                    Formulário
+                                </h3>
+                            </article>
+                            <ul>
+                                <li>Preenchimento rápido.</li>
+                                <li>Orçamento 100% assertivo em minutos.</li>
+                                <li>Ideal para quem já sabe o problema do veículo.</li>
+                            </ul>
+                            <article>
+                            <Button type="primary" href="/orcamento/formulario/:id" text="Gerar Orçamento" filled={true}/>
+                            </article>
+                        </div>
+                    </article>
+                </section>
+            </section>
         </main>
-    )
+    );
 }
